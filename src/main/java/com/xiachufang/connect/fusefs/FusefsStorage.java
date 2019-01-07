db043a21e47e4593463f9093522bfb06cf1e6978
cadl <ctrlaltdeleteliu@gmail.com>
2019-01-07 17:15:21 +0800

    checkstyle

--- src/main/java/com/xiachufang/connect/fusefs/FusefsStorage.java
+++ src/main/java/com/xiachufang/connect/fusefs/FusefsStorage.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2015 Xiachufang Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ **/
+
 package com.xiachufang.connect.fusefs;
 
 import com.xiachufang.connect.fusefs.wal.FusefsWAL;
@@ -9,11 +23,11 @@ import org.apache.kafka.common.TopicPartition;
 import java.io.IOException;
 
 public class FusefsStorage extends HdfsStorage {
-    public FusefsStorage(HdfsSinkConnectorConfig conf, String url) throws IOException {
-        super(conf, url);
-    }
+  public FusefsStorage(HdfsSinkConnectorConfig conf, String url) throws IOException {
+    super(conf, url);
+  }
 
-    public WAL wal(String topicsDir, TopicPartition topicPart) {
-        return new FusefsWAL(topicsDir, topicPart, this);
-    }
+  public WAL wal(String topicsDir, TopicPartition topicPart) {
+    return new FusefsWAL(topicsDir, topicPart, this);
+  }
 }
